9d8917a533984e1e7a0c02a1e48373e6a39524fd
Andrii Doroshko <Andr.dorosh4@gmail.com>
2024-11-26 19:23:16 -0500

    Added Web socket service

---
+++ Sources/DataModels/WebSocketDataModels/AtlasWebSocketRequest.swift
@@ -0,0 +1,22 @@
+//
+//  AtlasWebSocketRequest.swift
+//  AtlasSupportSDK
+//
+//  Created by Andrey Doroshko on 11/26/24.
+//
+
+import Foundation
+
+struct WebSocketRequest: Codable {
+    let channelId: String
+    let channelKind: String
+    let packetType: String
+    let payload: String
+    
+    enum CodingKeys: String, CodingKey {
+        case channelId = "channel_id"
+        case channelKind = "channel_kind"
+        case packetType = "packet_type"
+        case payload
+    }
+}
