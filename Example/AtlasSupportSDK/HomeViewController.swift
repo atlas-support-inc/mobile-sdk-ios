1c0f8523de08d6ba27feeacdbcd732663d71ff0b
Andrii Doroshko <Andr.dorosh4@gmail.com>
2024-12-06 13:05:02 -0500

    Update Demo app

--- Example/AtlasSupportSDK/HomeViewController.swift
+++ Example/AtlasSupportSDK/HomeViewController.swift
@@ -22,10 +22,15 @@ class HomeViewController: UIViewController {
         AtlasSDK.setAtlasSDKDelegate(self)
         
         configureLayout()
+        setupGesture()
+        
+        title = "Atlas chat example"
+        userIDTextField.text = userId
     }
     
     @objc func chatButtonAction() {
-        guard let atlassViewController = AtlasSDK.getAtlassViewController() else {
+        let botID = botIDTextField.text ?? ""
+        guard let atlassViewController = AtlasSDK.getAtlassViewController(botID) else {
             print("HomeViewController Error: Can not create AtlasSDK View Controller")
             return
         }
@@ -34,19 +39,23 @@ class HomeViewController: UIViewController {
     }
     
     @objc func identifyButtonAction() {
-        AtlasSDK.identify(userId: userId,
+        let userID = userIDTextField.text ?? ""
+        AtlasSDK.identify(userId: userID,
                           userHash: nil,
                           userName: nil,
                           userEmail: nil)
     }
     
+    @objc private func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
     @objc func updateCustomButtonAction() {
+        let customField = customFieldTextField.text ?? ""
         let ticketID = "b0357db3-b9b8-49a7-99e6-a235778c7312"
-        if let data = try? JSONEncoder().encode("Test string") {
+        if let data = try? JSONEncoder().encode(customField) {
             
-            let map = ["text" : data
-                     
-            ]
+            let map = ["text" : data]
             
             AtlasSDK.updateCustomField(ticketId: ticketID, data: map)
         }
@@ -58,27 +67,71 @@ class HomeViewController: UIViewController {
                                   image: UIImage(systemName: "gear"),
                                   tag: 2)
         
+        view.addSubview(userIDTextField)
+        view.addSubview(customFieldTextField)
+        view.addSubview(botIDTextField)
         view.addSubview(chatButton)
         view.addSubview(identifyButton)
         view.addSubview(updateCustomField)
         
+        
+        userIDTextField.translatesAutoresizingMaskIntoConstraints = false
+        customFieldTextField.translatesAutoresizingMaskIntoConstraints = false
+        botIDTextField.translatesAutoresizingMaskIntoConstraints = false
         chatButton.translatesAutoresizingMaskIntoConstraints = false
         identifyButton.translatesAutoresizingMaskIntoConstraints = false
         updateCustomField.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            identifyButton.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 50),
+            userIDTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            userIDTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            userIDTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            botIDTextField.topAnchor.constraint(equalTo: userIDTextField.bottomAnchor, constant: 16),
+            botIDTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            botIDTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            identifyButton.topAnchor.constraint(equalTo: botIDTextField.bottomAnchor, constant: 20),
             identifyButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
             
-            chatButton.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -50),
-            chatButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            customFieldTextField.topAnchor.constraint(equalTo: identifyButton.bottomAnchor, constant: 40),
+            customFieldTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            customFieldTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             
-            
-            updateCustomField.centerYAnchor.constraint(equalTo: view.layoutMarginsGuide.centerYAnchor),
+            updateCustomField.topAnchor.constraint(equalTo: customFieldTextField.bottomAnchor, constant: 20),
             updateCustomField.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
             
+            chatButton.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -50),
+            chatButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
         ])
     }
+    
+    private func setupGesture() {
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        swipeGesture.direction = [.down, .up]
+        view.addGestureRecognizer(swipeGesture)
+    }
+    
+    private let userIDTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter User ID"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let customFieldTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Custom 'TEXT' Field"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private let botIDTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Bot ID"
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
     
     private(set) lazy var chatButton = {
         let button = UIButton(type: .custom)
